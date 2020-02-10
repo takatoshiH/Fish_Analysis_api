119166dc3779af0b98a4cdb0257c8e90c6cf997c
takatoshiH <t.hikijiri@gmail.com>
2020-02-10 18:02:40 +0900

    潮位が記録できるようになった

--- app/models/tide.rb
+++ app/models/tide.rb
@@ -1,2 +1,22 @@
 class Tide < ApplicationRecord
+  require "httpclient"
+  require "json"
+  require "date"
+
+  def getter
+    prefecture_key = 40
+    port_key = 21
+    year = 2020
+    month = 2
+    day = 9
+    yesterday = (Date.today -1).to_s
+
+    api_url = "https://tide736.net/api/get_tide.php?pc=#{prefecture_key}&hc=#{port_key}&yr=#{year}&mn=#{month}&dy=#{day}&rg=day"
+    
+    responses = JSON.parse(HTTPClient.get(api_url).body)["tide"]["chart"][yesterday]["tide"]
+
+    responses.each do |response|
+      Tide.create(time: Time.at(response["unix"]), cm: response["cm"])
+    end
+  end
 end
--- test.rb
+++ test.rb
@@ -2,9 +2,20 @@ require "httpclient"
 require "json"
 require "date"
 
-api_url = "https://tide736.net/api/get_tide.php?pc=40&hc=21&yr=2020&mn=02&dy=09&rg=day"
+prefecture_key = 40
+port_key = 21
+year = 2020
+month = 2
+day = 9
+today = (Date.today-1).to_s
+puts today
+
+api_url = "https://tide736.net/api/get_tide.php?pc=#{prefecture_key}&hc=#{port_key}&yr=#{year}&mn=#{month}&dy=#{day}&rg=day"
 responses = HTTPClient.get(api_url).body
 # responses = JSON.parse(HTTPClient.get(api_url).body)
 
-puts JSON.parse(responses)["tide"]["chart"]["2020-02-09"]["tide"].first
-puts JSON.parse(responses)["tide"]["chart"]["2020-02-09"]["tide"][1]
+JSON.parse(responses)["tide"]["chart"][today]["tide"].each do |response|
+  puts Time.at(response["unix"])
+  puts response["cm"]
+end
+# puts JSON.parse(responses)["tide"]["chart"]["2020-02-09"]["tide"][1]
