3b6fd5da19c4af317a48a67c1721ba6110a80453
takatoshiH <t.hikijiri@gmail.com>
2020-02-10 16:27:58 +0900

    過去の気象情報が取得できるようになった

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -13,6 +13,15 @@ class Weather < ApplicationRecord
   def self.get_weather
     time = Time.parse((Date.today).to_s).to_i
     api_url = "https://api.darksky.net/forecast/#{@@app_id}/#{@@location},#{time}"
-    responses = JSON.parse(HTTPClient.get(api_url).body)["daily"]["data"].first
+    responses = JSON.parse(HTTPClient.get(api_url).body)
+
+    date =  Time.at(responses["currently"]["time"])
+    temperature = responses["currently"]["temperature"]
+    pressure = responses["currently"]["pressure"]
+    weather = responses["currently"]["summary"]
+    windSpeed = responses["currently"]["windSpeed"]
+    humidity = responses["currently"]["humidity"]
+
+    Weather.create(date: date, temperature: temperature, pressure: pressure, weather: weather, wind_speed: windSpeed, humidity: humidity)
   end
 end
