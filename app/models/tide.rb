d8c1d5464a481f7130cbb36acec2a0fbee919c75
takatoshiH <t.hikijiri@gmail.com>
2020-02-13 18:31:41 +0900

    細かい修正

--- app/models/tide.rb
+++ app/models/tide.rb
@@ -10,7 +10,7 @@ class Tide < ApplicationRecord
     day = yesterday.split("-")[2]
 
     api_url = "https://tide736.net/api/get_tide.php?pc=#{prefecture_key}&hc=#{port_key}&yr=#{year}&mn=#{month}&dy=#{day}&rg=day"
-    
+
     JSON.parse(HTTPClient.get(api_url).body)["tide"]["chart"][yesterday]["tide"].each do |response|
       Tide.create(time: Time.at(response["unix"]), cm: response["cm"])
     end
--- lib/tasks/weatehr_getter.rake
+++ lib/tasks/weatehr_getter.rake
@@ -1,6 +1,6 @@
 namespace :forecast_getter do
   task run: [:environment] do
     Weather.getter("33.36,130.25")
-    Tide.getter
+    Tide.getter(40,21)
   end
 end
